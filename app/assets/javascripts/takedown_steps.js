ab06f74c9af69c20d049a1180a165b911419a55c
Mark Oblad <moblad@valcu.co>
2014-02-08 17:40:22 -0500

    remove bootstap wizard

--- app/assets/javascripts/takedown_steps.js
+++ app/assets/javascripts/takedown_steps.js
@@ -2,13 +2,3 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-
-# wizard with options and events
-$(document).ready(function() {
-    $('#rootwizard').bootstrapWizard({
-        tabClass: 'nav nav-pills',
-        onNext: function(tab, navigation, index) {
-            alert('next');
-        }
-  });
-});
