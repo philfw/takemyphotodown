18179acbc96a4bec9b968c5b59eae2ce5948e0ec
Mark Oblad <moblad@valcu.co>
2014-02-08 18:02:36 -0500

    fix syntax in TakedownMailer

--- app/mailers/takedown_mailer.rb
+++ app/mailers/takedown_mailer.rb
@@ -8,12 +8,14 @@ class TakedownMailer < ActionMailer::Base
   	mail to: user.email, subject: "Sign Up Confirmation"
   end
 
-  def contact_valcu(email, subject="DMCA Takedown Notice and Request", body)
-  	@email = email
+  def contact_valcu(from="valcu.webmaster@gmail.com", to_emails, subject="DMCA Takedown Notice and Request", body)
+    @from = from
+  	@to_emails = to_emails
   	@subject = subject
   	@body = body
 
-
-  	mail to: "valcu.webmaster@gmail.com", subject: , from: email
+    @to_emails.each do |to_email|
+  	  mail to: to_email, subject: @subject, from: @from, body: @body
+    end
   end
 end
